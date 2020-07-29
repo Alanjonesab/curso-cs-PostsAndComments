0d27826a9b7181403be11ca4fa035ae675ebc905
Alanjonesab <alanjones_18@hotmail.com>
2020-07-29 10:45:47 -0300

    Exercicio sobre posts e comentários

---
+++ PostAndComments/Entitys/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostAndComments.Entitys
+{
+    class Comment
+    {
+        public string Text { get; set;}
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
